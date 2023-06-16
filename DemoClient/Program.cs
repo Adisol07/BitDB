c603cb8fd85eca638ead6f303e5292c61237bfb9
adambestajovsky <bestajovskyadam@seznam.cz>
2023-06-16 20:43:34 +0200

    Beta

--- DemoClient/Program.cs
+++ DemoClient/Program.cs
@@ -7,8 +7,8 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        ConnectionArgs connection = new ConnectionArgs("127.0.0.1", 44);
-        Console.WriteLine(Client.SendCommand("GET namebyvalue 'hustyyy'", connection).ToString());
+        ConnectionArgs connection = new ConnectionArgs("209.38.192.39", 44);
+        Console.WriteLine(Client.SendCommand("GET value 'test/testovacidoc/fff'", connection).ToString());
         Console.ReadLine();
     }
 }
